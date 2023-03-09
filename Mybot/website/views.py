dcd33c2c04bb9af671d438c89adec65a7fcb9be7
Zyr <zyr724725@gmail.com>
2023-03-09 17:36:59 +0800

    add list in views.py & add select options by for loop

--- Mybot/website/views.py
+++ Mybot/website/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    language_list = [' python', 'c', 'cpp', 'css', 'django', 'git', 'go', 'java', 'javascript', 'markup', 'mongodb', 'powershell', 'r', 'regex', 'ruby', 'rust', 'sql', 'swift']
+
+    return render(request, 'home.html', {'language_list':language_list})
 
