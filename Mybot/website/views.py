62bc49a5f853076787246d4074cc93dbd89ff7c7
Zyr <zyr724725@gmail.com>
2023-03-09 21:43:58 +0800

    add other languages supported by prism & submitting stuff from our form to the back end, doing something with it, sending it

--- Mybot/website/views.py
+++ Mybot/website/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 
 
 def home(request):
-    language_list = [' python', 'c', 'cpp', 'css', 'django', 'git', 'go', 'java', 'javascript', 'markup', 'mongodb', 'powershell', 'r', 'regex', 'ruby', 'rust', 'sql', 'swift']
+    language_list = ['bash', 'c', 'clike', 'cpp', 'csharp', 'css', 'csv', 'django', 'docker', 'git', 'go', 'http', 'java', 'javascript', 'json', 'json5', 'latex', 'markdown', 'markup', 'matlab', 'mongodb', 'php', 'powershell', 'python', 'r', 'regex', 'ruby', 'sql', 'swift', 'vim']
+    if request.method == "POST":
+        code = request.POST['code']
+        language = request.POST['language']
+        return render(request, 'home.html', {'language_list':language_list, 'code': code, 'language': language})
 
     return render(request, 'home.html', {'language_list':language_list})
 
