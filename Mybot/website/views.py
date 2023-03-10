173065488f257259a0612be5810b06d84c3300a9
Zyr <zyr724725@gmail.com>
2023-03-10 23:48:42 +0800

    add compiled bootstrap & change api from openai to writesonic

--- Mybot/website/views.py
+++ Mybot/website/views.py
@@ -1,24 +1,50 @@
 from django.shortcuts import render
 from django.contrib import messages
+import requests
+import os
 
 
 def home(request):
-
-    # 用户可选择的编程语言list
-    language_list = ['bash', 'c', 'clike', 'cpp', 'csharp', 'css', 'csv', 'django', 'docker', 'git', 'go', 'http', 'java', 'javascript', 'json', 'json5', 'latex', 'markdown', 'markup', 'matlab', 'mongodb', 'php', 'powershell', 'python', 'r', 'regex', 'ruby', 'sql', 'swift', 'vim']
     # 处理提交表单后得到的表单数据
     if request.method == "POST":
 
-        code = request.POST['code']
-        language = request.POST['language']
+        question = request.POST['question']
+        task = request.POST['task']
 
-        # 若用户未选择language就提交则给出提示
-        if language == 'Select Programming Language':
-            messages.success(request, "You should choose a programming language...")
+        print(task)
+
+        # 若用户未选择language就提交, 则给出提示
+        if task == '选择任务和A.I. Bot对话吧！':
+            messages.success(request, "你忘记选择任务了！")
             # 仍然要返回code，否则用户之前写的代码全没了会生气，不太友好
-            return render(request, 'home.html', {'language_list':language_list, 'code': code, 'language': language})
+            return render(request, 'home.html', {'question': question})
 
-        return render(request, 'home.html', {'language_list':language_list, 'code': code, 'language': language})
+        # 若用户选择任务并提交, 则调用writesonic api处理已提交的表单数据
+        else:
+            try:
 
-    return render(request, 'home.html', {'language_list':language_list})
+                # 以下参考Writesonic官方文档调用ChatSonic api
+                url = "https://api.writesonic.com/v2/business/content/chatsonic?engine=premium&language=zh"
+                key = os.getenv("WRITESONIC_API_KEY")
 
+                payload = {
+                    "enable_google_results": "true",
+                    "enable_memory": True,
+                    "input_text": question
+                }
+                headers = {
+                    "accept": "application/json",
+                    "content-type": "application/json",
+                    "X-API-KEY": key
+                }
+
+                response = requests.post(url, json=payload, headers=headers)
+                # 将chatsonic返回的结果转换为dict类型
+                response = eval(response.text)
+                # 获取返回的message, 即ChatSonic的回复
+                answer = response['message']
+                return render(request, 'home.html', {'task': task,  'question': question, 'answer': answer})
+            except Exception as e:
+                # 出错时给code传e, 使code区域显示error
+                return render(request, 'home.html', {'question': question})
+    return render(request, 'home.html')
